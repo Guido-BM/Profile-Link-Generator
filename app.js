f30aa9a2e2e2031d9bd80fa615c18d96dda68b66
Guido <guidoboninomuelas@gmail.com>
2023-10-21 13:31:06 +0200

    link list hecha, funcionalidad probada, scroll pulido. FALTA: Estilos de botones de link, dimensionar scroll boxes, visivilidad de scroll boxes, revision de la navBar, colores y estilos de la pagina

--- app.js
+++ app.js
@@ -1,48 +1,66 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  const template = ` 
-   
-      <div class="postedLink bg-body-tertiary p-2">
-              
-        <div class="d-flex flex-row justify-content-between">
-          <p>Link #1</p><button type="button" class="btn btn-light buttonRemovePost border-0">Remove</button>
-        </div>
-        <div>
-          <label class="w-100">
-            Plataforma:
-            <select class="form-select mb-3">
-              <option selected></option>
-              <option value="Github">Github</option>
-              <option value="YouTube">YouTube</option>
-              <option value="LinkedIn">LinkedIn</option>
-              <option value="Instagram"> Instagram </option>
-              <option value="Others">Others</option>
-            </select>
-          </label>
-        </div>
-        <div>
-          <label for="platform-url">Aquí tu link:</label>
-          <div class="input-group">
-            <span class="input-group-text" id="destinationLink">https://</span>
-            <input type="text" class="form-control" id="platform-url" /></div>
-        </div>
-      </div>
-  
-    `;
-
   const buttonCreateNewPost = document.querySelector("#buttonNewPost");
+  const buttonSavePost = document.querySelector("#buttonSavePost");
+  const formContainer = document.querySelector("#formContainer");
+  const linksContainer = document.querySelector("#links");
 
   buttonCreateNewPost.addEventListener("click", () => {
-    const formContainer = document.querySelector("#formContainer");
+    const platformOptions = `
+      <option selected></option>
+      <option value="Github">Github</option>
+      <option value="YouTube">YouTube</option>
+      <option value="LinkedIn">LinkedIn</option>
+      <option value="Instagram">Instagram</option>
+      <option value="Others">Others</option>
+    `;
 
-    const newPost = document.createElement("div");
-    newPost.innerHTML = template;
+    const newForm = document.createElement("div");
+    newForm.classList.add("postedLink", "bg-body-tertiary", "p-2");
+    newForm.innerHTML = `
+      <div class="d-flex flex-row justify-content-between">
+        <p>Link #${formContainer.children.length + 1}</p>
+        <button type="button" class="btn btn-light buttonRemovePost border-0">Remove</button>
+      </div>
+      <div>
+        <label class="w-100">
+          Plataforma:
+          <select class="form-select mb-3">${platformOptions}</select>
+        </label>
+      </div>
+      <div>
+        <label for="platform-url">Aquí tu link:</label>
+        <div class="input-group">
+          <span class="input-group-text" id="destinationLink">https://</span>
+          <input type="text" class="form-control" id="platform-url" />
+        </div>
+      </div>
+    `;
 
-    formContainer.appendChild(newPost);
+    formContainer.appendChild(newForm);
 
-    const buttonRemovePost = newPost.querySelector(".buttonRemovePost");
+    const buttonRemovePost = newForm.querySelector(".buttonRemovePost");
 
     buttonRemovePost.addEventListener("click", () => {
-      newPost.remove();
+      newForm.remove();
     });
+  });
+
+  buttonSavePost.addEventListener("click", () => {
+    const formElements = formContainer.querySelectorAll(".postedLink");
+    let buttonsContainer = "";
+
+    formElements.forEach((form, index) => {
+      const platformSelect = form.querySelector("select");
+      const urlInput = form.querySelector("input");
+
+      if (platformSelect.value && urlInput.value) {
+        buttonsContainer += `<a href="${
+          urlInput.value
+        }" class="btn btn-primary" style="margin-right: 10px; background-color: blue; color: white;">Link #${
+          index + 1
+        }</a>`;
+      }
+    });
+    linksContainer.innerHTML = buttonsContainer;
   });
 });
